141fabd9b13eb8887e49da2f3828483011f2015a
itsLys <keyn.shieda@gmail.com>
2025-12-30 16:10:52 +0100

    start class lexer

--- src/lexer.ts
+++ src/lexer.ts
@@ -7,8 +7,43 @@ const token: Token = {
 	position: 0
 }
 
-export function lex(content: string) : Token[] {
+// private line: number = 1, TODO: implement for err printing
+// private column: number = 0,
+class Lexer {
+	private index: number = 0;
+	private currChar: string = this.input[0];
+	constructor(
+		private input: string,
+	) { }
 
+	nextToken() {}
+	private advance() {
+		this.index++;
+		this.currChar = this.input[this.index];
+	}
+
+	private peek() {
+		return this.input[this.index + 1];
+	}
+
+	private skipWhitespace(): void {
+		let ch = this.currChar;
+		while (ch === ' ' || ch === '\n' || ch === '\t' || ch === '\r') {
+			this.advance();
+			ch = this.currChar;
+		}
+	}
+
+	private readString() { }
+	private readNumber() { }
+	private readKeyword() { }
+
+
+	// private error(msg: string) : void { }
+}
+
+export function lex(content: string): Token[] {
+	const lexer: Lexer = new Lexer(content);
 	console.log(content);
 	let t: Token[] = [];
 	t.push(token)
@@ -16,3 +51,5 @@ export function lex(content: string) : Token[] {
 }
 
 // str API
+
+
